5342638060c212f33367317c725146454c3d3fb6
phinn88 <nguyenngocphi0304@gmail.com>
2024-04-09 13:34:28 +0700

    header

---
+++ js/header.js
@@ -0,0 +1,21 @@
+const searchBox = document.querySelector('.search-box-mobile');
+const searchBtn = document.querySelector('.search-nav-bar');
+const search = document.querySelector('.search-mobile');
+const closeBtn = document.querySelector('.close-btn');
+
+searchBtn.addEventListener('click', function(){
+  console.log(search.classList.contains('active'));
+  if(search.classList.contains('active')){
+    search.classList.remove('active');
+    searchBox.value = '';
+  }
+  else {
+    search.classList.add('active');
+    searchBox.focus();
+  }
+})
+
+// searchBtn.addEventListener('click', function(){
+//   search.classList.remove('active');
+//   searchBox.value = '';
+// })
