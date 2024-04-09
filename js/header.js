0250c67047393366a6e7d12b098924b9d68f36ea
phinn88 <nguyenngocphi0304@gmail.com>
2024-04-10 04:14:02 +0700

    afs

--- js/header.js
+++ js/header.js
@@ -1,22 +1,31 @@
-const searchBox = document.querySelector('.search-box-mobile');
-const searchBtn = document.querySelector('.search-nav-bar');
-const search = document.querySelector('.search-mobile');
-const closeBtn = document.querySelector('.close-btn');
+// const searchBox = document.querySelector('.search-box-mobile');
+// const searchBtn = document.querySelector('.search-nav-bar');
+// const search = document.querySelector('.search-mobile');
+// const closeBtn = document.querySelector('.close-btn');
 
-searchBtn.addEventListener('click', function(){
-  console.log(search.classList.contains('active'));
-  if(search.classList.contains('active')){
-        search.classList.remove('active');
-        searchBox.value = '';
-  }
-  else {
-    search.classList.add('active');
-    searchBox.focus();
-  }
-})
+// searchBtn.addEventListener('click', function(){
+//   console.log(search.classList.contains('active'));
+//   if(search.classList.contains('active')){
+//         search.classList.remove('active');
+//         searchBox.value = '';
+//   }
+//   else {
+//     search.classList.add('active');
+//     searchBox.focus();
+//   }
+// })
 // searchBtn.addEventListener('click', function(){
 //   search.classList.remove('active');
 //   searchBox.value = '';
 // })
+function toggleSearch() {
+  var searchBox = document.querySelector('.search-box-mobile');
+  if(searchBox.classList.contains('active')) {
+      searchBox.classList.remove('active');
+  } else {
+      searchBox.classList.add('active');
+      searchBox.focus(); // Đặt con trỏ vào input khi mở rộng
+  }
+}
 
 
